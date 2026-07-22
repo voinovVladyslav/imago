a206307675854ea8eaf1cf16b4acd8b3ce9dfa9a
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-07-22 21:01:41 +0200

    feat(storage): define storage interface, test save method

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -4,14 +4,27 @@ package storage
 import (
 	"fmt"
 	"io"
+	"strings"
+
+	"github.com/google/uuid"
 )
 
 type FileStorage interface {
-	Save(f io.Reader) (id string)
-	Get()
-	Delete()
+	Save(f io.Reader) (id uuid.UUID)
+	Get(id uuid.UUID) (f io.Reader)
+	Delete(id uuid.UUID)
+}
+
+type LocalFileStorage struct{}
+
+func (s *LocalFileStorage) Save(f io.Reader) uuid.UUID {
+	id := uuid.Must(uuid.NewV7())
+	return id
 }
 
 func Run() {
 	fmt.Println("Hello from storage")
+	s := LocalFileStorage{}
+	reader := strings.NewReader("test")
+	fmt.Println(s.Save(reader))
 }
