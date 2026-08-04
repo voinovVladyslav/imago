fa7e76b08409464c7a88fa3cafbbd1fec6a626b7
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-08-04 15:30:55 +0200

    refactor(storage): remove unused func

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,40 +1,6 @@
 // Package storage handles file uploads/retrieval
 package storage
 
-import (
-	"context"
-	"fmt"
-	"io"
-	"strings"
-)
-
-func _Run() error {
-	db, err := NewDB("storage.sqlite3")
-	if err != nil {
-		return err
-	}
-	repo := NewLocalFileRepo(db)
-
-	ctx := context.Background()
-	record, err := repo.Save(ctx, strings.NewReader("test file contents"))
-	if err != nil {
-		return err
-	}
-	fmt.Println("Saved to db:", record)
-	f, err := repo.Get(ctx, record.ID)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	data, err := io.ReadAll(f)
-	if err != nil {
-		return err
-	}
-	fmt.Println("Got from file itself:", string(data))
-	err = repo.Delete(ctx, record.ID)
-	return err
-}
-
 func Run() error {
 	return InitServer()
 }
